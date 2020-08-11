b627f7581cd9ca5d2ebf190ae35d92aff3db7840
Simon97i <softimag08@gmail.com>
2020-08-11 09:11:29 +0200

    deployment setup folder

---
+++ build/precache-manifest.6804d4443e325816e70e36e6836f1ee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e34018e4dbfb9b843c65c2a12aecd0",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/index.html"
+  },
+  {
+    "revision": "30c3181f687271c9e53e",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/static/css/main.2cbe490d.chunk.css"
+  },
+  {
+    "revision": "560061baf0f6c624299a",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/static/js/2.e0cf06d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/static/js/2.e0cf06d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c3181f687271c9e53e",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/static/js/main.fce57c10.chunk.js"
+  },
+  {
+    "revision": "d5f43f8a9b3ba33f4ec4",
+    "url": "/SimonImanigirimpuhwe/react-searchBooks-app.git/static/js/runtime-main.97b6abbf.js"
+  }
+]);
